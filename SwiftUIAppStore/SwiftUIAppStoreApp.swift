52a541850fe0d657ce47f878e02636e947cf9f99
David de la Puente <david.puente@marbleinteractive.com>
2023-02-09 21:09:08 +0100

    Base project

---
+++ SwiftUIAppStore/SwiftUIAppStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppStoreApp.swift
+//  SwiftUIAppStore
+//
+//  Created by Marble Interactive SL on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
