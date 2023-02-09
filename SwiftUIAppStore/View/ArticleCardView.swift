6ac08a390cfd68e5160d1f2a1581552bb3b1fedd
David de la Puente <david.puente@marbleinteractive.com>
2023-02-09 21:55:53 +0100

    Add Close Button to the 'ArticleCardView'

--- SwiftUIAppStore/View/ArticleCardView.swift
+++ SwiftUIAppStore/View/ArticleCardView.swift
@@ -19,35 +19,56 @@ struct ArticleCardView: View {
     
     var body: some View {
         GeometryReader { geometry in
-            ScrollView {
-                VStack(alignment: .leading) {
-                    Image(uiImage: image)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: geometry.size.width, height: min(image.size.height/3, 500))
-                        .border(Color(.sRGB, red:150/255, green: 150/255, blue: 150/255, opacity: 0.1), width: isShowContent ? 0 : 1)
-                        .cornerRadius(15)
-                        .overlay {
-                            ArticleExcerptView(category: category,
-                                               headline: headline,
-                                               subHeadline: subHeadline,
-                                               isShowContent: $isShowContent)
-                            .cornerRadius(isShowContent ? 0 : 15)
+            ZStack(alignment: .topTrailing) {
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: geometry.size.width, height: min(image.size.height/3, 500))
+                            .border(Color(.sRGB, red:150/255, green: 150/255, blue: 150/255, opacity: 0.1), width: isShowContent ? 0 : 1)
+                            .cornerRadius(15)
+                            .overlay {
+                                ArticleExcerptView(category: category,
+                                                   headline: headline,
+                                                   subHeadline: subHeadline,
+                                                   isShowContent: $isShowContent)
+                                .cornerRadius(isShowContent ? 0 : 15)
+                            }
+                        
+                        //Content
+                        if isShowContent {
+                            Text(content)
+                                .foregroundColor(Color(.darkGray))
+                                .font(.system(.body, design: .rounded))
+                                .padding(.horizontal)
+                                .padding(.bottom, 50)
+                                .transition(.move(edge: .bottom))
                         }
-                    
-                    //Content
-                    if isShowContent {
-                        Text(content)
-                            .foregroundColor(Color(.darkGray))
-                            .font(.system(.body, design: .rounded))
-                            .padding(.horizontal)
-                            .padding(.bottom, 50)
-                            .transition(.move(edge: .bottom))
                     }
                 }
             }
+            .shadow(color: Color(.sRGB, red: 64/255, green: 64/255, blue: 64/255, opacity: 0.3), radius: isShowContent ? 0 : 15)
+            
+            if isShowContent {
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        withAnimation(.easeInOut) {
+                            isShowContent = false
+                        }
+                    } label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .font(.system(size: 26))
+                            .foregroundColor(.white)
+                            .opacity(0.7)
+                    }
+                }
+                .padding(.top, 50)
+                .padding(.trailing)
+            }
         }
-        .shadow(color: Color(.sRGB, red: 64/255, green: 64/255, blue: 64/255, opacity: 0.3), radius: isShowContent ? 0 : 15)
     }
 }
 
