9444a0a681944fee279853553637d735e0c8a4d4
David de la Puente <david.puente@marbleinteractive.com>
2023-02-09 22:29:20 +0100

    Finish App with animations cards

--- SwiftUIAppStore/ContentView.swift
+++ SwiftUIAppStore/ContentView.swift
@@ -8,21 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showContent = false
+    @State private var selectedArticleIndex: Int?
+    @Namespace var nsArticle
+    
     var body: some View {
-        ScrollView {
-            VStack(spacing: 40) {
-                TopBarView()
-                    .padding(.horizontal, 20)
-                
-                ForEach(sampleArticles.indices, id: \.self) { index in
-                    ArticleCardView(category: sampleArticles[index].category,
-                                    headline: sampleArticles[index].headline,
-                                    subHeadline: sampleArticles[index].subHeadline,
-                                    image: sampleArticles[index].image, content:
-                                        sampleArticles[index].content, isShowContent:.constant(false))
-                    .padding(.horizontal, 20)
-                    .frame(height: min(sampleArticles[index].image.size.height/3, 500))
+        ZStack(alignment: .top) {
+            ScrollView {
+                VStack(spacing: 40) {
+                    TopBarView()
+                        .padding(.horizontal, 20)
+                    
+                    ForEach(sampleArticles.indices, id: \.self) { index in
+                        ArticleCardView(category: sampleArticles[index].category,
+                                        headline: sampleArticles[index].headline,
+                                        subHeadline: sampleArticles[index].subHeadline,
+                                        image: sampleArticles[index].image, content:
+                                            sampleArticles[index].content, isShowContent: $showContent)
+                        .matchedGeometryEffect(id: index, in: nsArticle)
+                        .onTapGesture {
+                            withAnimation(.interactiveSpring(response: 0.35, dampingFraction: 0.65, blendDuration: 0.1)) {
+                                selectedArticleIndex = index
+                                showContent.toggle()
+                            }
+                        }
+                        .padding(.horizontal, 20)
+                        .frame(height: min(sampleArticles[index].image.size.height/3, 500))
+                    }
                 }
+            }
+            .opacity(showContent ? 0 : 1)
+            if showContent, let selectedArticleIndex {
+                ArticleCardView(
+                    category: sampleArticles[selectedArticleIndex].category,
+                    headline: sampleArticles[selectedArticleIndex].headline,
+                    subHeadline: sampleArticles[selectedArticleIndex].subHeadline,
+                    image: sampleArticles[selectedArticleIndex].image,
+                    content: sampleArticles[selectedArticleIndex].content,
+                    isShowContent: $showContent)
+                .ignoresSafeArea()
             }
         }
     }
