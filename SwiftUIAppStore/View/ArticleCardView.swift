357be4de850e1c5153681b2267f1132d89c3df91
David de la Puente <david.puente@marbleinteractive.com>
2023-02-09 21:49:24 +0100

    Fix problem with the imagen in 'ArticleCardView'

--- SwiftUIAppStore/View/ArticleCardView.swift
+++ SwiftUIAppStore/View/ArticleCardView.swift
@@ -18,30 +18,32 @@ struct ArticleCardView: View {
     @Binding var isShowContent: Bool
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-                Image(uiImage: image)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: min(image.size.height/3, 500))
-                    .border(Color(.sRGB, red:150/255, green: 150/255, blue: 150/255, opacity: 0.1), width: isShowContent ? 0 : 1)
-                    .cornerRadius(15)
-                    .overlay {
-                        ArticleExcerptView(category: category,
-                                           headline: headline,
-                                           subHeadline: subHeadline,
-                                           isShowContent: $isShowContent)
-                        .cornerRadius(isShowContent ? 0 : 15)
+        GeometryReader { geometry in
+            ScrollView {
+                VStack(alignment: .leading) {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: geometry.size.width, height: min(image.size.height/3, 500))
+                        .border(Color(.sRGB, red:150/255, green: 150/255, blue: 150/255, opacity: 0.1), width: isShowContent ? 0 : 1)
+                        .cornerRadius(15)
+                        .overlay {
+                            ArticleExcerptView(category: category,
+                                               headline: headline,
+                                               subHeadline: subHeadline,
+                                               isShowContent: $isShowContent)
+                            .cornerRadius(isShowContent ? 0 : 15)
+                        }
+                    
+                    //Content
+                    if isShowContent {
+                        Text(content)
+                            .foregroundColor(Color(.darkGray))
+                            .font(.system(.body, design: .rounded))
+                            .padding(.horizontal)
+                            .padding(.bottom, 50)
+                            .transition(.move(edge: .bottom))
                     }
-                
-                //Content
-                if isShowContent {
-                    Text(content)
-                        .foregroundColor(Color(.darkGray))
-                        .font(.system(.body, design: .rounded))
-                        .padding(.horizontal)
-                        .padding(.bottom, 50)
-                        .transition(.move(edge: .bottom))
                 }
             }
         }
@@ -51,7 +53,7 @@ struct ArticleCardView: View {
 
 struct ArticleCardView_Previews: PreviewProvider {
     static var previews: some View {
-        ArticleCardView(category: sampleArticles[0].category, headline: sampleArticles[0].headline, subHeadline: sampleArticles[0].subHeadline, image: sampleArticles[0].image, content: sampleArticles[0].content, isShowContent: .constant(false))
+        ArticleCardView(category: sampleArticles[1].category, headline: sampleArticles[1].headline, subHeadline: sampleArticles[1].subHeadline, image: sampleArticles[1].image, content: sampleArticles[1].content, isShowContent: .constant(false))
             .previewDisplayName("Card View (no Content)")
         
         ArticleCardView(category: sampleArticles[0].category, headline: sampleArticles[0].headline, subHeadline: sampleArticles[0].subHeadline, image: sampleArticles[0].image, content: sampleArticles[0].content, isShowContent: .constant(true))
